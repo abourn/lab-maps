97a1b4d31369c83434f54347d7afada9f5e55a32
Adam Bourn <abourn@uw.edu>
2017-05-02 15:59:18 -0700

    finish lab

--- LabMap/app/src/main/java/com/example/iguest/labmap/MapsActivity.java
+++ LabMap/app/src/main/java/com/example/iguest/labmap/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.iguest.labmap;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,8 +8,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,8 +43,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // LatLng sydney = new LatLng(-34, 151);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng fountainCoord = new LatLng(47.6538, -122.3078);
+        Marker fountain = mMap.addMarker(new MarkerOptions()
+                .position(fountainCoord)
+                .title("Drumheller Fountain")
+                .snippet("Ducks swim there")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+        );
+        LatLng mgh = new LatLng(47.655, -122.3078);
+        PolylineOptions plo = new PolylineOptions();
+
+        plo.add(mgh);
+
+        plo.add(fountainCoord);
+        plo.add(new LatLng(47.6543, -122.3080));
+        plo.add(new LatLng(47.6538, -122.3082));
+        plo.add(new LatLng(47.655, -122.3083));
+        plo.color(Color.argb(500, 212, 175, 55));
+        plo.width(25);
+        googleMap.addPolyline(plo);
     }
 }
